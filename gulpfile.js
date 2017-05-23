ffa479d7af45466e9a17f63e607858e1657f36a4
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-05-22 23:50:58 -0400

    Revert "check in lib assets"
    This reverts commit 9b6b267d1c7dc9cfc895998c8276a5ec1787e64f.

--- gulpfile.js
+++ gulpfile.js
@@ -37,10 +37,6 @@ gulp.task('lib', ['clean:lib'], function () {
             dest: paths.libDir + 'bootstrap/fonts'
         },
         {
-            src: paths.npmDir + 'bootstrap-sass/assets/stylesheets/bootstrap/*',
-            dest: paths.libDir + 'bootstrap/sass'
-        },
-        {
             src: [
                 paths.npmDir + 'font-awesome/**/css/font-awesome.min.css',
                 paths.npmDir + 'font-awesome/**/fonts/*'
