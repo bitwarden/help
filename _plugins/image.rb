0d0029a7a6b688766be82720c7109d08d867505b
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-05-23 07:58:44 -0400

    icon and image tag plugins

---
+++ _plugins/image.rb
@@ -0,0 +1,19 @@
+﻿module Jekyll
+  module Tags
+    class ImageTag < Liquid::Block
+      def initialize(tag_name, src)
+        super
+        src.strip!
+        @src = src
+      end
+
+      def render(context)
+        "<a href=\"#{src}\" target=\"_blank\">
+            <img class=\"img-responsive img-thumbnail\" src=\"#{src}\" />
+        </a>"
+      end
+    end
+  end
+end
+
+Liquid::Template.register_tag('image', Jekyll::Tags::ImageTag)
