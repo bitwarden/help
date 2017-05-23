f17690a54b309df60bc9616180bbed64a2fe51a8
Kyle Spearrin <kyle.spearrin@gmail.com>
2017-05-22 22:39:59 -0400

    design updates

---
+++ _plugins/liquid_pluralize.rb
@@ -0,0 +1,20 @@
+﻿# pluralize
+#
+# A Liquid filter to make it easy to form correct plurals.
+#
+# https://github.com/bdesham/pluralize
+
+module Pluralize
+    def pluralize(number, singular, plural = nil)
+        number = number.to_i
+        if number == 1
+            "#{singular}"
+        elsif plural.nil?
+            "#{singular}s"
+        else
+            "#{plural}"
+        end
+    end
+end
+
+Liquid::Template.register_filter(Pluralize)
